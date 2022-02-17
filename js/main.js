3749e236cc03bd4be44e9465d881bd9a7141a6ba
tommymcm <tommymcmichen@gmail.com>
2022-02-17 13:29:32 -0600

    Added content and fixed content

--- js/main.js
+++ js/main.js
@@ -4,72 +4,76 @@ var texts = ['Computer Scientist', 'Compiler Researcher', 'Computer Engineer'];
 var text = texts[0];
 var typing = true;
 var holding = false;
+var speed = 200;
 var cursor_status = true;
-var cursor_time = 200;
+var cursor_time = 2 * speed;
 var cursor_held = 0;
-var speed = 100;
-var hold_time = 2000;
+var full_hold_time = 1600;
+var empty_hold_time = 800;
+var hold_time = full_hold_time;
 var time_held = 0;
-var flip = false;
+var num_blinks = 3;
+var blinks = 0;
 
 function typeWriter() {
-  if (flip) {
-    flip = false;
-    if ( holding ) {
-      if ( time_held < hold_time ) {
-        time_held += speed;
-      } else {
+  if (holding) {
+    if (time_held < hold_time) {
+      time_held += speed;
+    } else {
+      time_held = 0;
+      holding = false;
+      typing = !typing;
+    }
+  } else {
+    if (typing) {
+      if (text_i >= text.length) {
+        hold_time = full_hold_time;
+        holding = true;
         time_held = 0;
-        holding = false;
       }
     } else {
-      if ( typing ) {
-        if ( text_i >= text.length ) {
-          typing = false; // start erasing
-          
-          holding = true;
-          time_held = 0;
-        }
+      if (text_i < 0) {
+        arr_i = (arr_i + 1) % (texts.length);
+        text = texts[arr_i];
+
+        hold_time = empty_hold_time;
+        holding = true;
+        time_held = 0;
+      }
+    }
+
+    if (!holding) {
+      if (typing) {
+        // type next letter
+        ++text_i;
       } else {
-        if ( text_i < 0 ) {
-          arr_i = (arr_i+1) % (texts.length);
-          text = texts[arr_i];
-          typing = true; // start typing next letter
-          
-          holding = true;
-          time_held = 0;
-        }
+        // erase next letter
+        --text_i;
       }
-
-      if ( !holding ) {
-        if ( typing ) {
-          // type next letter
-          ++text_i;
-        } else {
-          // erase next letter
-          --text_i;
-        }
-      }
-
-      document.getElementById("typewriter-title").innerHTML = text.substr(0, text_i);
     }
-  } else {
-    flip = true;
+
+    document.getElementById("typewriter-title").innerHTML = "<b> > </b>" + text.substr(0, text_i);
   }
 
-  if ( cursor_held < cursor_time ) {
-    cursor_held += speed;
-  } else {
-    if ( cursor_status ) {
-      document.getElementById("typewriter-title").style.boxShadow = "3px 0px var(--coral)";
-      cursor_status = false;
-      cursor_held = 0;
+  if (holding) {
+    if (cursor_held < cursor_time) {
+      cursor_held += speed;
     } else {
-      document.getElementById("typewriter-title").style.boxShadow = "0px 0px var(--coral)";
-      cursor_status = true;
-      cursor_held = 0;
+      cursor_status = !cursor_status;
     }
+  } else {
+    cursor_status = true;
   }
-  
+
+  if (cursor_status) {
+    document.getElementById("typewriter-title").style.boxShadow = "3px 0px var(--coral)";
+    cursor_status = false;
+    cursor_held = 0;
+  } else {
+    document.getElementById("typewriter-title").style.boxShadow = "0px 0px var(--coral)";
+    cursor_status = true;
+    cursor_held = 0;
+  }
+
   setTimeout(typeWriter, speed);
 }
